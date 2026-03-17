2b056d1ad56025351ea109ddcb46459e89794d8e
vtymo <vladyslav_tymoshchuk@trimble.com>
2026-03-17 15:14:17 +0200

    Алгоритми (частина 1)

---
+++ algorithms.js
@@ -0,0 +1,17 @@
+console.log("Hello JS");
+
+// log+TAB
+
+let username = prompt("Type name:");  // string
+console.log("My name is ", username);
+
+let age = +prompt("Enter your age:");       // number
+console.log("I am ", age, " years old.");
+
+console.log("Next year I will be ", age + 1, " years old.");
+
+let width = +prompt("Enter door width :");
+let height = +prompt("Enter door height :");
+
+let area = width * height;
+console.log("Door area is ", area, "cm^2");
